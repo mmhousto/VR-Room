ea2890bace2417c94931ef6389e1a12b8ff2dbd6
mmhousto <mmhouston6@live.com>
2022-07-29 11:34:11 -0500

    2.1 Extension Activities

---
+++ Assets/Scripts/BallBounce.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBounce : MonoBehaviour
+{
+
+    AudioSource audioSource;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(rb.velocity.magnitude);
+        audioSource.PlayOneShot(audioSource.clip, rb.velocity.magnitude);
+    }
+}
